56d570811eba07a5986446d5e5d208a5a42ca34f
Pishen Tsai <pishen02@gmail.com>
2021-12-06 18:15:44 +0800

    First commit

---
+++ build.sbt
@@ -0,0 +1,38 @@
+name := "bql"
+
+version := "0.1.0-SNAPSHOT"
+scalaVersion := "2.13.5"
+crossScalaVersions := Seq("2.13.5", "2.12.13")
+
+libraryDependencies ++= Seq(
+  "com.google.cloud" % "google-cloud-bigquery" % "1.137.1"
+)
+
+organization := "net.pishen"
+licenses += "Apache-2.0" -> url(
+  "https://www.apache.org/licenses/LICENSE-2.0.html"
+)
+homepage := Some(url("https://github.com/pishen/bql"))
+scmInfo := Some(
+  ScmInfo(
+    url("https://github.com/pishen/bql"),
+    "scm:git@github.com:pishen/bql.git"
+  )
+)
+developers := List(
+  Developer(
+    id = "pishen",
+    name = "Pishen Tsai",
+    email = "",
+    url = url("https://github.com/pishen")
+  )
+)
+versionScheme := Some("early-semver")
+publishTo := {
+  val nexus = "https://oss.sonatype.org/"
+  if (isSnapshot.value) {
+    Some("snapshots" at nexus + "content/repositories/snapshots")
+  } else {
+    Some("releases" at nexus + "service/local/staging/deploy/maven2")
+  }
+}
---
+++ src/main/scala/bql/BQL.scala
@@ -0,0 +1,58 @@
+package bql
+
+import com.google.cloud.bigquery.QueryJobConfiguration
+import com.google.cloud.bigquery.QueryParameterValue
+import com.google.cloud.bigquery.StandardSQLTypeName
+
+import java.lang
+import scala.collection.JavaConverters._
+
+case class BQL(statement: String, params: Seq[QueryParameterValue]) {
+  def queryConfigBuilder =
+    QueryJobConfiguration
+      .newBuilder(statement)
+      .setPositionalParameters(params.asJava)
+  def queryConfig = queryConfigBuilder.build()
+}
+
+object BQL {
+  def create(sc: StringContext, args: Any*) = {
+    val parts = sc.parts
+    assert(parts.size == args.size + 1)
+    val stmt = parts.tail.zip(args).foldLeft(parts.head) {
+      case (stmt, (part, bql: BQL)) =>
+        stmt + bql.statement + part
+      case (stmt, (part, _)) =>
+        stmt + "?" + part
+    }
+    val params = args.flatMap {
+      case bql: BQL   => bql.params
+      case v: Boolean => Seq(QueryParameterValue.bool(v))
+      case v: String  => Seq(QueryParameterValue.string(v))
+      case v: Int     => Seq(QueryParameterValue.int64(v.toLong))
+      case v: Long    => Seq(QueryParameterValue.int64(v))
+      case v: Double  => Seq(QueryParameterValue.float64(v))
+      case v: Float   => Seq(QueryParameterValue.float64(v.toDouble))
+      case s @ Seq(_: Boolean, _*) =>
+        val arr = s.asInstanceOf[Seq[Boolean]].map(lang.Boolean.valueOf).toArray
+        Seq(QueryParameterValue.array(arr, StandardSQLTypeName.BOOL))
+      case s @ Seq(_: String, _*) =>
+        val arr = s.asInstanceOf[Seq[String]].toArray
+        Seq(QueryParameterValue.array(arr, StandardSQLTypeName.STRING))
+      case s @ Seq(_: Int, _*) =>
+        val arr = s.asInstanceOf[Seq[Int]].map(lang.Integer.valueOf).toArray
+        Seq(QueryParameterValue.array(arr, StandardSQLTypeName.INT64))
+      case s @ Seq(_: Long, _*) =>
+        val arr = s.asInstanceOf[Seq[Long]].map(lang.Long.valueOf).toArray
+        Seq(QueryParameterValue.array(arr, StandardSQLTypeName.INT64))
+      case s @ Seq(_: Double, _*) =>
+        val arr = s.asInstanceOf[Seq[Double]].map(lang.Double.valueOf).toArray
+        Seq(QueryParameterValue.array(arr, StandardSQLTypeName.FLOAT64))
+      case s @ Seq(_: Float, _*) =>
+        val arr = s.asInstanceOf[Seq[Float]].map(lang.Float.valueOf).toArray
+        Seq(QueryParameterValue.array(arr, StandardSQLTypeName.FLOAT64))
+      case _ => sys.error("Unsupported parameter type in bql.")
+    }
+    BQL(stmt.stripMargin, params)
+  }
+}
